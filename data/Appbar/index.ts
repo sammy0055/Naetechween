2d4ba8b306036826291a504c9e29a9e66bcb5bf4
sammy0055 <ronicksamuel@gmail.com>
2022-12-27 23:35:26 +0100

    added id to appbar data

--- data/Appbar/index.ts
+++ data/Appbar/index.ts
@@ -1,18 +1,22 @@
 export const AppbarData = [
   {
+    id:0,
     name: "Home",
     url: "#",
   },
   {
+    id:1,
     name: "Components",
     url: "#",
   },
   {
+    id:2,
     name: "Pages",
     url: "#",
     icon:"^"
   },
   {
+    id:3,
     name: "Documents",
     url: "#",
   },
