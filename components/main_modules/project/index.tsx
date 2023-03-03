0defda9dbcf3fae1aad9849b77578082f52ec8ba
sammy0055 <ronicksamuel@gmail.com>
2023-03-03 10:21:35 +0100

    added contributions to project section

--- components/main_modules/project/index.tsx
+++ components/main_modules/project/index.tsx
@@ -1,6 +1,17 @@
 import BigCta from "../../atom/btn/BigCta";
 import { Bigheading, MediumHeading, SmHeading } from "../../atom/headings/text";
-import { Box1, Box2, Pwrapper, Mheading, ContributionListwrapper, List } from "./project_components";
+import {
+  Box1,
+  Box2,
+  Pwrapper,
+  Mheading,
+  ContributionListwrapper,
+  List,
+  Unorderlist,
+  Previewbox,
+  Boxwrapper,
+  IframBox,
+} from "./project_components";
 import { BsArrowRight } from "react-icons/bs";
 import Image from "next/image";
 
@@ -8,12 +19,13 @@ const Project: React.FC = () => {
   return (
     <>
       <Pwrapper>
-        .
-        <div style={{backgroundColor:"#1f262e"}}>
-        <SmHeading style={{textAlign:"center"}}>Projects</SmHeading>
+        .<SmHeading style={{ textAlign: "center" }}>Projects</SmHeading>
+        <Boxwrapper>
           <Box1>
             <Mheading>Kindroom</Mheading>
-            <Bigheading>A live rent free by providing <br /> value platform</Bigheading>
+            <Bigheading>
+              A live rent free by providing <br /> value platform
+            </Bigheading>
             {/* <BigCta
               type="button"
               text="visit webpage"
@@ -21,25 +33,46 @@ const Project: React.FC = () => {
               showIcon
             /> */}
             <ContributionListwrapper>
-            <ul>
-              <List>setup page security</List>
-              <List>setup page security</List>
-              <List>setup page security</List>
-              <List>setup page security</List>
-            </ul>
+              <Unorderlist>
+                <List>
+                  Monitored system performance and availability, proactively
+                  identifying and resolving issues to ensure that the system
+                  runs smoothly.
+                </List>
+                <List>
+                  Stayed up-to-date with emerging technologies, trends, and best
+                  practices in web development, and share knowledge with the
+                  team.
+                </List>
+                <List>Developed new features and functionalities</List>
+                <List>
+                  Reviewed and provide feedback on other developers' code.
+                </List>
+              </Unorderlist>
 
-            <ul>
-              <List>setup page security</List>
-              <List>setup page security</List>
-              <List>setup page security</List>
-              <List>setup page security</List>
-            </ul>
+              <Unorderlist>
+                <List>Optimized system performance and scalability.</List>
+                <List>Ensure system security and data protection.</List>
+                <List>
+                  Collaborated with cross-functional teams, such as product
+                  management and design, to ensure that the product meets
+                  business requirements and user needs.
+                </List>
+                <List>
+                  Maintained and improve the codebase through refactoring, bug
+                  fixing, and continuous improvement.
+                </List>
+              </Unorderlist>
             </ContributionListwrapper>
           </Box1>
           <Box2>
-            <Image src={""} alt="work sample" />
+            <Previewbox>
+              <IframBox>
+              </IframBox>
+            </Previewbox>
+            {/* <Image src={""} alt="work sample" /> */}
           </Box2>
-        </div>
+        </Boxwrapper>
       </Pwrapper>
     </>
   );
--- components/main_modules/project/project_components.ts
+++ components/main_modules/project/project_components.ts
@@ -31,11 +31,45 @@ export const Mheading = styled(MediumHeading)`
 `;
 
 export const ContributionListwrapper = styled.div`
+  width: 100%;
   display: flex;
+  justify-content: space-evenly;
+`;
+
+export const Unorderlist = styled.ul`
+  width: 50%;
 `;
 
 export const List = styled.li`
-margin-top: 12px;
-margin-bottom: 12px;
-color: ${(props: { theme: theme }) => props.theme.textColors.primary};
-`
+  margin-top: 12px;
+  margin-bottom: 12px;
+  color: ${(props: { theme: theme }) => props.theme.textColors.primary};
+`;
+
+export const Previewbox = styled.div`
+  width: 90%;
+  height: 70vh;
+  background: #132f4c;
+  margin-top: 8rem;
+  margin-left: 12px;
+  border-radius: 3ch;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const IframBox = styled.div`
+  width: 80%;
+  height: 50vh;
+  background: white;
+  border-radius: 1ch;
+`;
+
+
+
+export const Boxwrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background: #1f262e;
+`;
